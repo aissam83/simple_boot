a9f68fa9b66fde99cbda89fb2db967c9acfd2466
didier-tp <didier@d-d-efrance.fr>
2020-01-24 14:26:45 +0100

    refactoring spring boot ws

--- src/main/java/com/formation/ctrl/CalulatorController.java
+++ src/main/java/com/formation/ctrl/CalulatorController.java
@@ -1,6 +1,8 @@
 package com.formation.ctrl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.formation.service.Calculator;
@@ -11,8 +13,8 @@ public class CalulatorController {
 	@Autowired
 	private Calculator calculator;
 	
-	public String sum(Integer a, Integer b) {
+	@RequestMapping("/sum")
+	public String sum(@RequestParam("a") Integer a,@RequestParam("b") Integer b) {
 		return String.valueOf(calculator.sum(a, b));
 	}
-
 }
